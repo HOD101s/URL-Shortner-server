27e1393d76ef816d43f59f497bab796801ffc413
HOD101s <manasbass.99@gmail.com>
2021-07-02 15:03:54 +0530

    added: body parsing + morgan logging

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const express = require('express');
+const morgan = require('morgan');
+
+// express Routers
 const apiRoutes = require('./routes/api.routes');
-const app = express();
 
 // Constant port for service
 const PORT = 5000;
 
+// express app
+const app = express();
+
+// Middleware
+
+// body parsing
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// logger
+app.use(morgan('tiny'));
+
+// Routers
 // adding api router
 app.use('/api', apiRoutes);
 
+// Server
 // Starting server on specified port
 app.listen(PORT, () => {
 	console.log(`Server running on Port ${PORT}`);

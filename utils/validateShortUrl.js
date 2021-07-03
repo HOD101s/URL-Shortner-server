cc3fd532c1d81b75c19edbfa10510f00de538031
HOD101s <manasbass.99@gmail.com>
2021-07-04 02:08:57 +0530

    modifed: eqeq bug

--- utils/validateShortUrl.js
+++ utils/validateShortUrl.js
@@ -1,5 +1,5 @@
 module.exports = validateShortUrl = (req, res, next) => {
-	if (req.originalUrl.length != 11) {
+	if (req.originalUrl.length !== 11) {
 		res.status(404);
 		res.json('Invalid Url');
 		res.end();
